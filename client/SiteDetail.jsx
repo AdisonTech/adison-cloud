627a0db68925f46c1ee3ba41fbe0fec1781861c8
Cliff Brake <cbrake@bec-systems.com>
2015-08-25 15:08:41 -0400

    UI work to display nodes

--- client/SiteDetail.jsx
+++ client/SiteDetail.jsx
@@ -1,9 +1,36 @@
 
+Bulb = React.createClass({
+  getInitialState() {
+    return {on: false};
+  },
+  handleOnOff() {
+    this.setState({on: !this.state.on});
+  },
+  handleBrightness() {
+  },
+  render() {
+    var node = this.props.node;
+    var bulbIcon = this.state.on ? '/icons/light-bulb-on.png' : '/icons/light-bulb.png';
+    return (
+      <ListItem
+        leftIcon = <img src={bulbIcon}/>
+        primaryText = {node.friendlyName}
+        rightIcon = <img src='/icons/brightness.png'/>
+        onTouchTap = {this.handleOnOff}
+      /> 
+    )
+  }
+});
+
 Node = React.createClass({
   render() {
-    return (
-      <ListItem primaryText={this.props.node.friendlyName} />
-   )
+    var node = this.props.node;
+
+    if (node.type == 'bulb') {
+      return <Bulb node={node} />;
+    } else {
+      return <ListItem primaryText={node.friendlyName} />;
+    }
   }
 });
 
@@ -26,6 +53,10 @@ SiteDetail = React.createClass({
     }
   },
 
+  handleLeftButton() {
+    FlowRouter.go('/');
+  },
+
   render() {
     console.log(this.data.site);
     var nodes = this.data.site.nodes.map(function(n) {
@@ -33,7 +64,7 @@ SiteDetail = React.createClass({
     });
     return (
       <div>
-        <AppBar title={this.data.site.name}/>
+        <AppBar title={this.data.site.name} onLeftIconButtonTouchTap={this.handleLeftButton}/>
         <List>{nodes}</List>
       </div>
     )
